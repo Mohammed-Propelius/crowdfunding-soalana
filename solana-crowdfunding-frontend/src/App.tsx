a71dab4e1aabd47c99adc38bf922de11db804c00
Mohammed Kharoda <kharodawalam@gmail.com>
2023-08-11 10:25:50 +0530

    inital commit

---
+++ solana-crowdfunding-frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  const checkIfWalletIsConnected = async () => {
+    try { 
+      const {solana} = window.
+    }
+    catch (e) {
+      console.log(e)
+    }
+  }
+}
+
+export default App
